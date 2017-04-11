7795e4336ffa98f8d11041b383024d59e4c5b6b7
master <master@LAP003>
2017-04-11 15:06:50 -0500

    asdas

--- Suplacorp.GPS.DAL/DAL.cs
+++ Suplacorp.GPS.DAL/DAL.cs
@@ -10,14 +10,17 @@ namespace Suplacorp.GPS.DAL
     {
         //ENCRIPTAR ESTA CADENA A LA MANERA SUPLACORINA(LUEGO).
         public static string CreateConnection(string psRegValueName) {
-            //DESARROLLO
+            //DESARROLLO SERVIDOR DESARROLLO
             //return "Data Source=10.7.2.132;Initial Catalog=SuplacorpDB;User Id=sa;Password=S8p5A2o7P";
 
-            //DESARROLLO
+            //DESARROLLO SERVIDOR SUPLACORPDB
             //return "Data Source=10.7.2.251;Initial Catalog=SuplacorpDB_BBVA;User Id=sa;Password=S8p5A2o7P";
 
             //PRODUCCIÓN
             return "Data Source=10.7.2.251;Initial Catalog=SuplacorpDB;User Id=sa;Password=S8p5A2o7P";
+
+
+
         }
     }
 }
